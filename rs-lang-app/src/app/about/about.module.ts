4e3f7a21b80bcc1d5c2893ccb6d0c9290d233050
IvanMakarishchev <denis.flank@gmail.com>
2022-08-24 14:59:27 +0300

    feat: add top elements, add font styles to app.component.scss, add router-wrapper class to app.component.scss, set margin to 0 in styles.css

--- rs-lang-app/src/app/about/about.module.ts
+++ rs-lang-app/src/app/about/about.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './components/about/about.component';
 import { SharedModule } from '../shared/shared.module';
+import { AuthorizationModule } from '../authorization/authorization.module';
 
 @NgModule({
   declarations: [AboutComponent],
-  imports: [CommonModule, AboutRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    AboutRoutingModule,
+    SharedModule,
+    AuthorizationModule,
+  ],
 })
 export class AboutModule {}
