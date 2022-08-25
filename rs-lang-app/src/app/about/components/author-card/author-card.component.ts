87902dbd41852a06710bfe26ad7c00bff6d9497d
IvanMakarishchev <denis.flank@gmail.com>
2022-08-25 12:27:05 +0300

    feat: add author-card component files, add authors.json file

---
+++ rs-lang-app/src/app/about/components/author-card/author-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-author-card',
+  templateUrl: './author-card.component.html',
+  styleUrls: ['./author-card.component.scss'],
+})
+export class AuthorCardComponent {
+  constructor() {}
+}
