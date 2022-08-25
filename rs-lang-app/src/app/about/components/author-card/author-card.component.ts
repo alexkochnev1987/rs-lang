82eab1b39232a68a84c9338b87ad4982e0ecec8b
IvanMakarishchev <denis.flank@gmail.com>
2022-08-25 15:06:16 +0300

    feat: make different border colors for author photo, add some styles for author text

--- rs-lang-app/src/app/about/components/author-card/author-card.component.ts
+++ rs-lang-app/src/app/about/components/author-card/author-card.component.ts
@@ -16,10 +16,19 @@ interface authorCard {
   styleUrls: ['./author-card.component.scss'],
 })
 export class AuthorCardComponent implements OnInit {
+  private borderColors = ['#88E564', '#FFB84D', '#D698EC', '#42BEE5'];
+
   @Input()
   authors!: authorCard[];
 
   ngOnInit() {
     this.authors = authorData;
+  }
+
+  setAuthorPhotoStyle(author: authorCard) {
+    return {
+      'background-image': `url('${author.photoURL}')`,
+      'border-color': `${this.borderColors[author.id]}`,
+    };
   }
 }
