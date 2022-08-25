43ffc579f66b23515524046e7ff10a85ae159d5c
IvanMakarishchev <denis.flank@gmail.com>
2022-08-25 20:40:30 +0300

    feat: make footer look like in design

--- rs-lang-app/src/app/shared/components/footer/footer.component.ts
+++ rs-lang-app/src/app/shared/components/footer/footer.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import authorsData from '../../../authors.json';
+import { Component, Input, OnInit } from '@angular/core';
+interface authorCard {
+  id: number;
+  firstName: string;
+  secondName: string;
+  photoURL: string;
+  git: string;
+  features: string[];
+  aboutAuthor: string;
+}
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {
-  constructor() {}
+export class FooterComponent implements OnInit {
+  @Input()
+  authors!: authorCard[];
+
+  ngOnInit() {
+    this.authors = authorsData;
+  }
+
+  getGitIcon(author: authorCard) {
+    return {
+      'background-image': `url('../../../assets/img/footer_git_${author.id}.svg')`,
+    };
+  }
 }
